9dcf34859b66d96c9e1ef2442ed7676ae1c8cc7f
matt murphy <mattyhead@gmail.com>
2017-01-30 15:14:38 -0500

    reshaping a bit -- that iframe isn't self closing

--- site/views/output/tmpl/default.php
+++ site/views/output/tmpl/default.php
@@ -5,16 +5,11 @@ $hash=$this->hash;
 $unpublish=($hash) ? "&task=unpublish&cid[]=$id&hash=$hash" : '';
 
 ?>
-<p><?=JText::_('REJECT MESSAGE START');?>
-    <a href="<?=JRoute::_('index.php?option=com_pvnominations&view=input' . $unpublish);?>">
-        <?=JText::_('REJECT MESSAGE here');?>.
-    </a>
-    <?=JText::_('DOWNLOAD MESSAGE START');?>
-    <a href="<?=JRoute::_('index.php?option=com_pvnominations&view=output&format=raw&download=true&id=' . $id);?>" download >
-        <?=JText::_('DOWNLOAD MESSAGE HERE');?>
-    </a>.
-</p>
 <p>
-    <?=JText::_('DOWNLOAD RELATED FILES');?>
+    <?=JText::_('REJECT MESSAGE START');?>
+    <a href="<?=JRoute::_('index.php?option=com_pvnominations&view=input' . $unpublish);?>"><?=JText::_('REJECT MESSAGE here');?></a>.
+    <?=JText::_('DOWNLOAD MESSAGE START');?>
+    <a href="<?=JRoute::_('index.php?option=com_pvnominations&view=output&format=raw&download=true&id=' . $id);?>" download ><?=JText::_('DOWNLOAD MESSAGE HERE');?></a>.
 </p>
-<iframe width="100%" height="700px" src="<?=JRoute::_('index.php?option=com_pvnominations&view=output&format=raw&id=' . $id);?>" />
+<p><?=JText::_('DOWNLOAD RELATED FILES');?></p>
+<iframe width="100%" height="700px" src="<?=JRoute::_('index.php?option=com_pvnominations&view=output&format=raw&id=' . $id);?>"></iframe>
