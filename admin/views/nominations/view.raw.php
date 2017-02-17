77feb8d7d391561db4289d5dcc08051ec3bf1330
matt murphy <mattyhead@gmail.com>
2017-02-17 07:45:27 -0500

    adding export wiring

---
+++ admin/views/nominations/view.raw.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Pvnominations View for Pvnominations Component
+ *
+ * @package    Joomla.Tutorials
+ * @subpackage Components
+ * @link http://docs.joomla.org/Developing_a_Model-View-Controller_Component_-_Part_4
+ * @license        GNU/GPL
+ */
+
+// No direct access
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Pvnominations View
+ *
+ * @package    Joomla.Tutorials
+ * @subpackage Components
+ */
+class PvnominationsViewNominations extends JView
+{
+    /**
+     * Pvnominations view display method
+     * @return void
+     **/
+    public function display($tpl = 'export')
+    {
+
+        // Get data from the model
+
+        $items = &$this->get('Data');
+
+        if (!count($items)) {
+            $mainframe = JFactory::getApplication();
+            $mainframe->redirect('index.php?option=com_pvnominations', 'You cannot export an empty result set.');
+        }
+
+        $this->assignRef('items', $items);
+
+        parent::display($tpl);
+    }
+}
